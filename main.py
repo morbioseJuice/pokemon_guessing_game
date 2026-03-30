df8b5c31752c9e8231a14a8adc9ac182e47ac742
morbioseJuice <willjbriggs7@gmail.com>
2026-03-30 16:23:02 -0600

    1. Added new util, yes_no_input that manages if user correctly types yes/no
    2. Added get_pokemon_types
    
    3. Modified print_all_pokemon to have an optional print types
    
    4. Made a question class with guess_types_by_name and guess_number_by_name for quizzing.
    
    5. Added a menu to select Play, View Pokemon, or Quit
    
    6. Added case 1 to call both or either of the quiz functions. It tells you if you got the types/numbers right then tells you the correct one. It repeats forever.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import json
+import random
 
 # Because the json has 4 languages, why not have the option to change it lol
 language = "english"
@@ -41,6 +42,17 @@ class utils:
             except ValueError:
                 print("Invalid number.")
 
+    def yes_no_input(prompt):
+        while True:
+            user_input = input(prompt)
+
+            if user_input == "yes":
+                return True
+            elif user_input == "no":
+                return False
+            else:
+                continue
+
 # A class as a container for several functions that read data from the pokedex variable to spit out pokemon information
 class pokedex_retrieval_functions:
     # Enter in a Pokemon #, and it spits out its name
@@ -63,10 +75,87 @@ class pokedex_retrieval_functions:
                 print(found_number)
                 return found_number
 
-    # Prints all pokemon from pokedex variable - 2 optional arguments allow for a range of pokemon
     @staticmethod
-    def print_all_pokemon(start=1,stop=len(pokedex)):
-        for index, pokemon in enumerate(pokedex[start - 1 : stop]):
-            print(f"{pokemon["name"][language]} - #{pokemon["id"]}")
+    def get_pokemon_types(pokemon_number):
+        for pokemon in pokedex:
+            if pokemon["id"] == pokemon_number:
+                return pokemon["type"]
 
-pokedex_retrieval_functions.print_all_pokemon()
+    # Prints all pokemon from pokedex variable - 5 optional arguments allow for a range of pokemon, and can decide whether to print names, types, or numbers
+    @staticmethod
+    def print_all_pokemon(start=1,stop=len(pokedex), q_print_types = True):
+        for index, pokemon in enumerate(pokedex[start - 1 : stop]):
+            if q_print_types == True:
+                print(f"{pokemon["name"][language]} - {pokemon["id"]} --- {pokemon["type"]}")
+            else:
+                print(f"{pokemon["name"][language]} - {pokemon["id"]} ")
+
+class questions:
+    @staticmethod
+    def guess_types_by_name(id):
+        name = pokedex_retrieval_functions.get_pokemon_name(id)
+        types = [p.lower() for p in pokedex_retrieval_functions.get_pokemon_types(id)]
+
+        utils.clear_console()
+
+        guess_types = [
+            input(f"Guess {name}'s first type: ").lower(),
+            input(f"Guess {name}'s second type (leave blank if it has none): ").lower(),
+        ]
+
+        try:
+            guess_types.remove("")
+        except ValueError:
+            pass
+
+        if sorted(types) == sorted(guess_types):
+            return [True, types]
+        else:
+            return [False, types]
+
+    @staticmethod
+    def guess_number_by_name(id):
+        name = pokedex_retrieval_functions.get_pokemon_name(id)
+
+        utils.clear_console()
+
+        guess_id = utils.int_input(f"Guess {name}'s Pokedex #: ")
+
+        if id == guess_id:
+            return [True, id]
+        else:
+            return [False, id]
+
+while True:
+    print("Pokemon Guessing Game by Will\n")
+    print("Choose an option: ")
+    print("1. Play")
+    print("2. View Pokemon")
+    print("3. Quit\n")
+    opt = utils.int_input("? ", 1, 3)
+
+    match opt:
+        case 1:
+            should_ask_num = utils.yes_no_input("Would you like to be quizzed on Pokedex #? (yes/no)")
+            should_ask_types = utils.yes_no_input("Would you like to be quizzed on Pokemon Type? (yes/no)")
+
+            while True:
+                if should_ask_num:
+                    return_val = questions.guess_number_by_name(random.randint(0, len(pokedex) - 1))
+
+                    print(f"You got the answer right" if return_val[0] else "You got the answer wrong")
+                    print(f"The correct answer was {return_val[1]}")
+
+                if should_ask_types:
+                    return_val = questions.guess_types_by_name(random.randint(0, len(pokedex) - 1))
+
+                    print(f"You got the answer right" if return_val[0] else "You got the answer wrong")
+                    print(f"The correct answer was {return_val[1]}")
+
+                utils.wait_for_enter()
+
+        case 2:
+            pokedex_retrieval_functions.print_all_pokemon()
+            utils.wait_for_enter()
+        case 3:
+            exit()
